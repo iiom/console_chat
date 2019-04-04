973daabbadc79478e6efe7797477ad5372d0833b
iiom <ioma766@gmail.com>
2019-04-04 17:59:34 +0300

    fix cycles sign_login & read_write

--- console_chat.rb
+++ console_chat.rb
@@ -9,37 +9,41 @@ DBConnection.db_configuration(db_configuration_file)
 
 def sign_login
   choice = nil
-  until choice == 1 || choice == 2
-    puts "Выберите регистрация или авторизация\nregistr - 1\nlogin - 2"
-    choice = STDIN.gets.chomp.to_i
-    if choice == 1
-      puts 'Введите ваше Имя(login name)'
-      name = STDIN.gets.chomp
-      puts 'email'
-      email = STDIN.gets.chomp
-      user = User.new(name: name, email: email)
-      user.save
-      if user.errors.present?
-        choice = nil
-        user.errors.each {|i| p i}
-      else
-        puts "Регистрация #{user.name} завершена\n\n"
-      end
-    elsif choice == 2
-      puts 'Введите email'
-      email = STDIN.gets.chomp
-      user = User.find_by(email: email)
-      if user.nil?
-        choice = nil
-        puts 'Такого имени нет в базе'
-      else
-        puts "Авторизация #{user.name} успешна\n\n"
-      end
+  puts "Выберите регистрация или авторизация\nregistr - 1\nlogin - 2\nexit - 9"
+  choice = STDIN.gets.chomp
+
+  if choice.to_i == 9
+    puts 'bye bye'
+  elsif choice.to_i == 1
+    puts 'Введите ваше Имя(login name)'
+    name = STDIN.gets.chomp
+    puts 'Введите ваш (email)'
+    email = STDIN.gets.chomp
+    user = User.create(name: name, email: email)
+    if user.errors.present?
+      p user.errors.each {|i| i}
+      sign_login
+    else
+      puts "Регистрация #{user.name} завершена\n\n"
     end
+  elsif choice.to_i == 2
+    puts 'Введите email'
+    email = STDIN.gets.chomp
+    user = User.find_by(email: email)
+    if user.nil?
+      puts 'Такого имени нет в базе'
+      sign_login
+    else
+      puts "Авторизация #{user.name} успешна\n\n"
+    end
+  else
+    puts "wrong input - #{choice}"
+    sign_login
   end
+  read_wright_message(user)
 end
 
-def read_right_message
+def read_wright_message(user)
   choice = nil
   puts '1 - написать сообщение'
   puts '2 - прочитать сообщения адресованные пользователю'
@@ -47,7 +51,7 @@ def read_right_message
   puts '9 - выход'
   choice = STDIN.gets.chomp
 
-  if choice == 9
+  if choice.to_i == 9
     puts 'bye bye'
   elsif choice.to_i == 1
     puts 'enter message'
@@ -56,17 +60,18 @@ def read_right_message
     whom = STDIN.gets.chomp
     whom = nil if whom == ''
     Message.create(text: text, user: user, whom: whom)
+    read_wright_message(user)
   elsif choice.to_i == 2
     Message.where(whom: user.name).each {|i| puts i.text}
-    read_right_message
+    read_wight_message(user)
   elsif choice.to_i == 3
     Message.where(user_id: user.id).each {|i| puts i.text}
-    read_right_message
+    read_wright_message(user)
   else
     puts "wrong input - #{choice}"
-    read_right_message
+    read_wright_message(user)
   end
 end
 
 sign_login
-read_right_message
+
